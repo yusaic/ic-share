40663c9a2a7ebc76004e98f69f74e8bca2a53739
unknown <tito@PC-1608-04.infinity-c.sjk>
2019-06-07 19:24:08 +0900

    Change Layout

--- Add_Perm.ps1
+++ Add_Perm.ps1
@@ -1,30 +1,22 @@
-
 ## マイドキュメント →2003
 ## MyDocuments      →2016
 
 
-## 変数
-################################################################
+## 変数 ##
+###################################################################################################################
 
-## 旧2003 Serverのパス
-$KATAKANA1 = 'C:\ic-share\Administrator/ドキュメント'
+$KATAKANA1 = 'C:\ic-share\Administrator/ドキュメント' ## 旧2003 Serverのパス
+$EIGO1     = 'C:\ic-share\Administrator/MyDocuments'  ## 新2016 Serverのパス
+$user      = 'administrator'                          ## 各ユーザ名
+$owner     = 'FILESERVER\Administrator'               ## 所有者のパス（ドメイン＋ファイルサーバのローカルアドミン）
+$domain    = 'FILESERVER'                             ## 新2016 Serverのドメイン名
 
-## 新2016 Serverのパス
-$EIGO1 = 'C:\ic-share\Administrator/MyDocuments'
-
-## 各ユーザ名
-$user = 'administrator'
-
-## 所有者のパス（ドメイン＋ファイルサーバのローカルアドミン）
-$owner = 'FILESERVER\Administrator' 
-
-## 新2016 Serverのドメイン名
-$domain = 'FILESERVER'
-
-################################################################
+###################################################################################################################
 
 
 
+## メインコード ##
+##############################################################################################
 
 ## 1.マイドキュメントの所有者をファイルサーバのローカルアドミンに変更
 icacls $KATAKANA1 /setowner $owner /T
@@ -33,19 +25,15 @@ icacls $KATAKANA1 /setowner $owner /T
 takeown /F $KATAKANA1 /R
 
 ## 2.MyDocumentsを削除
-
 Remove-Item $EIGO1 -Force -Recurse
 
 ## 3.マイドキュメントをMyDocumentsにRename
 Rename-Item $KATAKANA1 $EIGO1
 
 ## 4.空フォルダ「マイドキュメント」を作成
-
 New-Item $KATAKANA1 -Type directory
 
-
 ## 5.MyDocumentsのショートカット作成
-
     $create_shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut
     $TargetPath = "$KATAKANA1"
     $Shortcut = "$EIGO1.lnk"
@@ -58,6 +46,5 @@ New-Item $KATAKANA1 -Type directory
     Move-Item $Shortcut $TargetPath
 
 ## 6.新ドメイン各ユーザの権限をフォルダ内の全ファイルにつける
-
 cmd /C "icacls $EIGO1\* /grant $domain\$user`:F /T"
 
