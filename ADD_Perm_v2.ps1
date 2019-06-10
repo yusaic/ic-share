c2e7d95a52eda8b9303da69a70bb87147a4dd1ac
unknown <tito@PC-1608-04.infinity-c.sjk>
2019-06-10 11:52:39 +0900

    Create ADD_Perm_v2.ps1

---
+++ ADD_Perm_v2.ps1
@@ -0,0 +1,50 @@
+## マイドキュメント →2003
+## MyDocuments      →2016
+
+
+## 変数 ##
+###################################################################################################################
+
+$KATAKANA1 = 'C:\ic-share\Administrator/ドキュメント' ## 旧2003 Serverのパス
+$EIGO1     = 'C:\ic-share\Administrator/MyDocuments'  ## 新2016 Serverのパス
+$user      = 'administrator'                          ## 各ユーザ名
+$owner     = 'FILESERVER\Administrator'               ## 所有者のパス（ドメイン＋ファイルサーバのローカルアドミン）
+$domain    = 'FILESERVER'                             ## 新2016 Serverのドメイン名
+
+###################################################################################################################
+
+
+
+## メインコード ##
+##############################################################################################
+
+## 1.マイドキュメントの所有者をファイルサーバのローカルアドミンに変更
+icacls $KATAKANA1 /setowner $owner /T
+
+##所有者確認コマンド
+takeown /F $KATAKANA1 /R
+
+## 2.MyDocumentsを削除
+Remove-Item $EIGO1 -Force -Recurse
+
+## 3.マイドキュメントをMyDocumentsにRename
+Rename-Item $KATAKANA1 $EIGO1
+
+## 4.空フォルダ「マイドキュメント」を作成
+New-Item $KATAKANA1 -Type directory
+
+## 5.MyDocumentsのショートカット作成
+    $create_shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut
+    $TargetPath = "$KATAKANA1"
+    $Shortcut = "$EIGO1.lnk"
+    $s = $create_shortcut.invoke("$Shortcut") # Must end in .lnk
+    $s.TargetPath = "$EIGO1"
+    $s.IconLocation = "imageres.dll,3" # This is a reference to a folder icon
+    $s.Description = "My Folder"
+    $s.Save()
+    
+    Move-Item $Shortcut $TargetPath
+
+## 6.新ドメイン各ユーザの権限をフォルダ内の全ファイルにつける
+cmd /C "icacls $EIGO1\* /grant $domain\$user`:F /T"
+
