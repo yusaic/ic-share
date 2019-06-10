ae63159f4e45df6b0967743133fe944cd04c48e3
test-user1 <yusaic1@gmail.com>
2019-06-10 17:59:26 +0900

    ADD_Perm_v2.ps1の細部修正

--- ADD_Perm_v2.ps1
+++ ADD_Perm_v2.ps1
@@ -1,20 +1,20 @@
-## マイドキュメント →2003
-## MyDocuments      →2016
+## ドキュメント →2003
+## Documents    →2016
 
 
 ## Listファイルからユーザ名を読み込む
 foreach ($user in Get-Content list.txt) {
 
 ## 処理中のユーザ名を表示 
-Write-Output "Processing $User ...."
+Write-Output "Processing $user ...."
 
 
 ## 変数 ##
 ###################################################################################################################
 
-# $user      = <LISTファイルから取得したユーザ名>     ## 各ユーザ名(Listファイルから取得)
+# $user    = <LISTファイルから取得したユーザ名>       ## 各ユーザ名(Listファイルから取得)
 $KATAKANA1 = "<file serverのunc>\$user\ドキュメント"  ## 旧2003 Serverのパス
-$EIGO1     = "<file serverのunc>\$user\MyDocuments"   ## 新2016 Serverのパス
+$EIGO1     = "<file serverのunc>\$user\Documents"     ## 新2016 Serverのパス
 $owner     = 'FILESERVER\Administrator'               ## 所有者のパス（ドメイン＋ファイルサーバのローカルアドミン）
 $domain    = 'FILESERVER'                             ## 新2016 Serverのドメイン名
 
@@ -25,22 +25,22 @@ $domain    = 'FILESERVER'                             ## 新2016 Serverのドメ
 ## メインコード ##
 ##############################################################################################
 
-## 1.マイドキュメントの所有者をファイルサーバのローカルアドミンに変更
+## 1.ドキュメントの所有者をファイルサーバのローカルアドミンに変更
 icacls $KATAKANA1 /setowner $owner /T
 
 ##所有者確認コマンド
 takeown /F $KATAKANA1 /R
 
-## 2.MyDocumentsを削除
+## 2.Documentsを削除
 Remove-Item $EIGO1 -Force -Recurse
 
-## 3.マイドキュメントをMyDocumentsにRename
+## 3.ドキュメントをDocumentsにRename
 Rename-Item $KATAKANA1 $EIGO1
 
-## 4.空フォルダ「マイドキュメント」を作成
+## 4.空フォルダ「ドキュメント」を作成
 New-Item $KATAKANA1 -Type directory
 
-## 5.MyDocumentsのショートカット作成
+## 5.Documentsのショートカット作成
 $create_shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut
 $TargetPath = "$KATAKANA1"
 $Shortcut = "$EIGO1.lnk"
@@ -49,7 +49,7 @@ $s.TargetPath = "$EIGO1"
 $s.IconLocation = "imageres.dll,3" # This is a reference to a folder icon
 $s.Description = "My Folder"
 $s.Save()
-## ショートカットファイルをMyDocumentsに移動 
+## ショートカットファイルをDocumentsに移動 
 Move-Item $Shortcut $TargetPath
 
 ## 6.新ドメイン各ユーザの権限をフォルダ内の全ファイルにつける
