a5947eb3c156ecc165edaac24dec0303adeaf8fa
test-user1 <yusaic1@gmail.com>
2019-06-10 18:59:45 +0900

    path修正

--- 1_mkdirv2.ps1
+++ 1_mkdirv2.ps1
@@ -1,11 +1,11 @@
 ﻿## フォルダ作成
-for($i=190; $i -lt 193; $i++){
+for($i=190; $i -lt 201; $i++){
 $x = "vabb0$i"
 mkdir $x
 }
 
 ## 資料等移動
-for($i=190; $i -lt 193; $i++){
+for($i=190; $i -lt 201; $i++){
     $x = "vabb0$i"
     cp -R ドキュメント $x  
     cp -R Documents $x
@@ -13,7 +13,7 @@ for($i=190; $i -lt 193; $i++){
 
 ## ディレクトリの下に説明書きテキストを作る
 $dir="<file serverのunc>"
-for($i=190; $i -lt 193; $i++){
+for($i=190; $i -lt 201; $i++){
     $x = "vabb0$i"
     New-Item -Type File "$dir\$x\ドキュメント\ドキュメント_2003.txt"
     New-Item -Type File "$dir\$x\Documents\document_2016.txt"
