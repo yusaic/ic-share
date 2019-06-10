439dfdd06856f4d99b9472f7a9b2f78bc9cd3c95
test-user1 <yusaic1@gmail.com>
2019-06-10 17:38:24 +0900

    TEST_ADD_Perm.ps1　細部修正

---
+++ 1_mkdirv2.ps1
@@ -0,0 +1,20 @@
+﻿## フォルダ作成
+for($i=190; $i -lt 193; $i++){
+$x = "vabb0$i"
+mkdir $x
+}
+
+## 資料等移動
+for($i=190; $i -lt 193; $i++){
+    $x = "vabb0$i"
+    cp -R ドキュメント $x  
+    cp -R Documents $x
+    }    
+
+## ディレクトリの下に説明書きテキストを作る
+$dir="<file serverのunc>"
+for($i=190; $i -lt 193; $i++){
+    $x = "vabb0$i"
+    New-Item -Type File "$dir\$x\ドキュメント\ドキュメント_2003.txt"
+    New-Item -Type File "$dir\$x\Documents\document_2016.txt"
+}
--- TEST_ADD_Perm.ps1
+++ TEST_ADD_Perm.ps1
@@ -1,12 +1,13 @@
-## マイドキュメント →2003
-## MyDocuments      →2016
+﻿## ドキュメント →2003
+## Documents    →2016
 
 
 ## Listファイルからユーザ名を読み込む
 foreach ($user in Get-Content list.txt) {
 
 ## 処理中のユーザ名を表示 
-Write-Output "Processing $User ...."
+#表示されない
+Write-Output "Processing $user ...."
 
 
 ## 変数 ##
@@ -14,7 +15,7 @@ Write-Output "Processing $User ...."
 
 # $user      = <LISTファイルから取得したユーザ名>     ## 各ユーザ名(Listファイルから取得)
 $KATAKANA1 = "<file serverのunc>\$user\ドキュメント"  ## 旧2003 Serverのパス
-$EIGO1     = "<file serverのunc>\$user\MyDocuments"   ## 新2016 Serverのパス
+$EIGO1     = "<file serverのunc>\$user\Documents"     ## 新2016 Serverのパス
 $owner     = 'FILESERVER\Administrator'               ## 所有者のパス（ドメイン＋ファイルサーバのローカルアドミン）
 $domain    = 'FILESERVER'                             ## 新2016 Serverのドメイン名
 
@@ -25,7 +26,7 @@ $domain    = 'FILESERVER'                             ## 新2016 Serverのドメ
 ## メインコード ##
 ##############################################################################################
 
-## 1.マイドキュメントの所有者をファイルサーバのローカルアドミンに変更
+## 1.ドキュメントの所有者をファイルサーバのローカルアドミンに変更
 $x1 = "icacls $KATAKANA1 /setowner $owner /T"
 echo "$x1"
 
@@ -33,32 +34,37 @@ echo "$x1"
 $x2 = "takeown /F $KATAKANA1 /R"
 echo "$x2"
 
-## 2.MyDocumentsを削除
+## 2.Documentsを削除
 $x3 = "Remove-Item $EIGO1 -Force -Recurse"
 echo "$x3"
 
-## 3.マイドキュメントをMyDocumentsにRename
+## 3.ドキュメントをMyDocumentsにRename
 $x4 = "Rename-Item $KATAKANA1 $EIGO1"
 echo "$x4"
 
-## 4.空フォルダ「マイドキュメント」を作成
+## 4.空フォルダ「ドキュメント」を作成
 $x5 = "New-Item $KATAKANA1 -Type directory"
 echo "$x5"
 
+
+#ここから下はユーザーのフォルダを作成したあとに実施#
+#####################################################
+
 ## 5.MyDocumentsのショートカット作成
 
-#$create_shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut
-#$TargetPath = "$KATAKANA1"
-#$Shortcut = "$EIGO1.lnk"
-#$s = $create_shortcut.invoke("$Shortcut") # Must end in .lnk
-#$s.TargetPath = "$EIGO1"
-#$s.IconLocation = "imageres.dll,3" # This is a reference to a folder icon
-#$s.Description = "My Folder"
-#$s.Save()
+$create_shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut
+$TargetPath = "$KATAKANA1"
+$Shortcut = "$EIGO1.lnk"
+$s = $create_shortcut.invoke("$Shortcut") # Must end in .lnk
+$s.TargetPath = "$EIGO1"
+$s.IconLocation = "imageres.dll,3" # This is a reference to a folder icon
+$s.Description = "My Folder"
+$s.Save()
 ## ショートカットファイルをMyDocumentsに移動 
-#Move-Item $Shortcut $TargetPath
+Move-Item $Shortcut $TargetPath
 
 ## 6.新ドメイン各ユーザの権限をフォルダ内の全ファイルにつける
 $y ="cmd /C `"icacls $EIGO1\* /grant $domain\$user`:F /T`""
 echo "$y"
+
 }
