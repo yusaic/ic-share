e38c33f97916080363ca8e44d3c0bc0a701cdaa1
unknown <tito@PC-1608-04.infinity-c.sjk>
2019-06-11 10:21:42 +0900

    ADD_Perm_v4が最新

---
+++ 1_mkdirv2_ito.ps1
@@ -0,0 +1,36 @@
+﻿## フォルダ削除
+for($i=500; $i -lt 510; $i++){
+$x = "vabb0$i"
+rm -r $x
+}
+
+## フォルダ作成
+for($i=500; $i -lt 510; $i++){
+$x = "vabb0$i"
+mkdir $x
+}
+
+## 資料等移動
+for($i=500; $i -lt 510; $i++){
+    $x = "vabb0$i"
+    cp -R ドキュメント $x  
+    cp -R Documents $x
+    }    
+
+## ディレクトリの下に説明書きテキストを作る
+$dir="\\172.31.242.221\ic-share"
+for($i=500; $i -lt 510; $i++){
+    $x = "vabb0$i"
+    New-Item -Type File "$x\ドキュメント\ドキュメント_2003.txt"
+    New-Item -Type File "$x\Documents\document_2016.txt"
+}
+
+## ファイルサーバの管理者の権限を削除
+for($i=500; $i -lt 510; $i++){
+    $x = "vabb0$i"
+    echo "ICACLS C:\ic-share\$x /inheritance:d | ICACLS C:\ic-share\$x /remove:g BUILTIN\Administrators"
+    ICACLS C:\ic-share\$x /inheritance:d | ICACLS C:\ic-share\$x /remove:g BUILTIN\Users
+    ICACLS C:\ic-share\$x /inheritance:d | ICACLS C:\ic-share\$x /remove:g BUILTIN\Administrators
+    ICACLS C:\ic-share\$x /inheritance:d | ICACLS C:\ic-share\$x /remove:g FILESERVER\Administrator
+    ICACLS C:\ic-share\$x /inheritance:d | ICACLS C:\ic-share\$x /remove:g EVERYONE
+    }   
---
+++ ADD_Perm_If.ps1
@@ -0,0 +1,63 @@
+﻿## マイドキュメント →2003
+## MyDocuments      →2016
+
+
+## Listファイルからユーザ名を読み込む
+foreach ($user in Get-Content list.txt) {
+
+## 処理中のユーザ名を表示 
+Write-Output "Processing $User ...."
+
+
+## 変数 ##
+###################################################################################################################
+
+# $user      = <LISTファイルから取得したユーザ名>     ## 各ユーザ名(Listファイルから取得)
+$KATAKANA1 = "<file serverのunc>\$user\ドキュメント"  ## 旧2003 Serverのパス
+$EIGO1     = "<file serverのunc>\$user\MyDocuments"   ## 新2016 Serverのパス
+$owner     = 'FILESERVER\Administrator'               ## 所有者のパス（ドメイン＋ファイルサーバのローカルアドミン）
+$domain    = 'FILESERVER'                             ## 新2016 Serverのドメイン名
+
+###################################################################################################################
+
+
+
+## メインコード ##
+##############################################################################################
+
+## ユーザ入力
+$YesOrNo = (Read-Host マイドキュメントの所有者を変更しますか？ y/n)
+
+if ( $YesOrNo -ne "y" ) { exit }
+## 1.マイドキュメントの所有者をファイルサーバのローカルアドミンに変更
+icacls $KATAKANA1 /setowner $owner /T
+
+##所有者確認コマンド
+takeown /F $KATAKANA1 /R
+
+## 2.MyDocumentsを削除
+Remove-Item $EIGO1 -Force -Recurse
+if ( $YesOrNo -ne "y" ) { exit }
+
+## 3.マイドキュメントをMyDocumentsにRename
+Rename-Item $KATAKANA1 $EIGO1
+
+## 4.空フォルダ「マイドキュメント」を作成
+New-Item $KATAKANA1 -Type directory
+
+## 5.MyDocumentsのショートカット作成
+$create_shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut
+$TargetPath = "$KATAKANA1"
+$Shortcut = "$EIGO1.lnk"
+$s = $create_shortcut.invoke("$Shortcut") # Must end in .lnk
+$s.TargetPath = "$EIGO1"
+$s.IconLocation = "imageres.dll,3" # This is a reference to a folder icon
+$s.Description = "My Folder"
+$s.Save()
+## ショートカットファイルをMyDocumentsに移動 
+Move-Item $Shortcut $TargetPath
+
+## 6.新ドメイン各ユーザの権限をフォルダ内の全ファイルにつける
+cmd /C "icacls $EIGO1\* /grant $domain\$user`:F /T"
+
+}
